01c62495fc4e873dc06acf2812ed6f8192eb81a5
Xuefang Zhao <xz974@eris1n3.research.partners.org>
2017-10-27 11:37:44 -0400

    updated svtools

---
+++ svtools/svtools/standardize/__init__.py
@@ -0,0 +1,6 @@
+from .standardize import VCFStandardizer
+from .std_delly import DellyStandardizer
+from .std_wham import WhamStandardizer
+from .std_lumpy import LumpyStandardizer
+from .std_manta import MantaStandardizer
+from .std_melt import MeltStandardizer
